b9373066d849a24d37bb9c87c956ebc80cde1246
Kayam-Reis25 <81162483+Kayam-Reis25@users.noreply.github.com>
2021-05-06 09:31:03 -0300

    Update ADDPulo.js
    Falta ainda adicionar gravidade, só tá se movendo pra baixo pra cima por enquanto

--- Programacao/ADDPulo.js
+++ Programacao/ADDPulo.js
@@ -1,89 +1,48 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<style>
-canvas {
-    border: 1px solid #d3d3d3;
-    background-color: #f1f1f1;
-}
-</style>
-</head>
-<body onload="startGame()">
-<script>
+var canvas = document.querySelector('canvas');
+canvas.width = 1350;
+canvas.height = 610;
+var c = canvas.getContext('2d');
+var tecla;
 
-var myGamePiece;
+window.addEventListener('keyup', function(event) {
+    tecla = event.key;
+})
+var bolinha = new Ball(390,390,10,3,3,0);
+function Ball (x,y,radius, color) {
+  
+   this.x = x;
+   this.y = y;
+   this.radius = radius;
+   this.color = color;
+  
 
-function startGame() {
-  myGameArea.start();
-  myGamePiece = new component(25, 25, "red", 50, 120);
-}
+  this.desenhar = function (){
+        c.beginPath();
+        c.strokeStyle = '#00AA00';
+        c.fillStyle = this.color;
+        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        c.stroke();
+        c.fill();
+    }
+     
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keydown', function (e) {
-            myGameArea.key = e.keyCode;
-        })
-        window.addEventListener('keyup', function (e) {
-            myGameArea.key = false;
-        })
+  this.pular = function () {
+        if((tecla=='w') || (tecla=='W')) {
+                this.y = this.y - 15;
+                tecla = '';
+            }
+       if((tecla=='s') || (tecla=='S')) {
+                this.y = this.y + 10;
+                tecla = '';
+            }
+        } 
+
         
-    },
-    stop : function() {
-        clearInterval(this.interval);
-    },    
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+ } 
+ function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,1350,610);
+    bolinha.desenhar();
+    bolinha.pular();
 }
-
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;    
-    this.gravity = 0.05;
-    this.gravitySpeed = 0;
-    this.update = function() {
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }  
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-        }
-    }
-}
-
-function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;    
-    if (myGameArea.key && myGameArea.key == 65) {myGamePiece.speedX = -5; }
-    if (myGameArea.key && myGameArea.key == 68) {myGamePiece.speedX = 5; }
-    if (myGameArea.key && myGameArea.key == 87) {myGamePiece.speedY = -10; }
-    if (myGameArea.key && myGameArea.key == 83) {myGamePiece.speedY = 5; }
-    myGamePiece.newPos();    
-    myGamePiece.update();
-}
-
-
-</script>
-</body>
-</html>
+animate();
