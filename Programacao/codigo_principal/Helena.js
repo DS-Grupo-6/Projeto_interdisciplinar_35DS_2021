0a155b49b9971bc80f252c9e89a55fba8e990308
Marco Fonseca <marcomapafrjjr@gmail.com>
2021-05-09 20:48:06 -0300

    Add do codigo Helena.js comentado

--- Programacao/codigo_principal/Helena.js
+++ Programacao/codigo_principal/Helena.js
@@ -1,7 +1,7 @@
-var canvas = document.querySelector('canvas');
-canvas.width = 1350;
-canvas.height = 610;
-var c = canvas.getContext('2d');
+var canvas = document.querySelector('canvas');//chama o canvas do html
+canvas.width = 1350; //define a largura do canvas
+canvas.height = 610; //define o tamanho do canva 
+var c = canvas.getContext('2d'); // define o contexto do canvas para 2d
 
 var tecla;
 var shift;
@@ -42,15 +42,15 @@ window.addEventListener('keyup', function(event) {
     tecla = '';
 })
 
-var imgBackground = new addImagem("sprites_cenario/BG.png", 0, 0, canvas.width, canvas.height);
-var sorveteria = new addImagem("sprites_cenario/sorveteria.png",0,148,457,334 ); 
-var chao = new addImagem("sprites_cenario/chao1.png",0,482,640,128);
-var caixa = new addImagem("sprites_cenario/Crate.png",574,418,64,64);
-var plat = new addImagem("sprites_cenario/plat.png",672,290,192,64);
-var pico = new addImagem("sprites_cenario/pico.png",960,96,64,256);
-var plat1 = new addImagem("sprites_cenario/plat1.png",1158,322,192,64);
-var arv = new addImagem("sprites_cenario/Tree_2.png",380,226,256,256);
-var arb = new addImagem("sprites_cenario/Bush (1).png",380,430,106.4,52)
+var imgBackground = new addImagem("sprites_cenario/BG.png", 0, 0, canvas.width, canvas.height); //define a imagem de backgraund com o mesmo tamanho do canvas
+var sorveteria = new addImagem("sprites_cenario/sorveteria.png",0,148,457,334 ); //coloca o sprite da sorveteria no canva com posições definidadas pelo objeto addImagem
+var chao = new addImagem("sprites_cenario/chao1.png",0,482,640,128); //coloca o sprite do chão no canva com posições definidadas pelo objeto addImagem
+var caixa = new addImagem("sprites_cenario/Crate.png",574,418,64,64); //coloca o sprite da caixa no canva com posições definidadas pelo objeto addImagem
+var plat = new addImagem("sprites_cenario/plat.png",672,290,192,64); //coloca o sprite do plataforma1 no canva com posições definidadas pelo objeto addImagem
+var pico = new addImagem("sprites_cenario/pico.png",960,96,64,256); //coloca o sprite d "pico" no canva com posições definidadas pelo objeto addImagem
+var plat1 = new addImagem("sprites_cenario/plat1.png",1158,322,192,64); //coloca o sprite da plataforma2 no canva com posições definidadas pelo objeto addImagem
+var arv = new addImagem("sprites_cenario/Tree_2.png",380,226,256,256); //coloca o sprite da árvore no canva com posições definidadas pelo objeto addImagem
+var arb = new addImagem("sprites_cenario/Bush (1).png",380,430,106.4,52) //coloca o sprite do arbusto no canva com posições definidadas pelo objeto addImagem
 
 var coracao1 = new addImagem("hud_sprites/cora_cheio.png", 1100, 60, 50, 50);
 var coracao2 = new addImagem("hud_sprites/cora_cheio.png", 1125, 60, 50, 50);
@@ -166,7 +166,7 @@ var objColisao = [chao, caixa, plat, pico, plat1];
 
 var Helena = new personagem(sprites[0],148,356,83,122,3,3,300)
 
-function addImagem(src, posX, posY, width, height) {
+function addImagem(src, posX, posY, width, height) { //objeto que facilita a adição de imagens no canva
     this.image = new Image();
     this.image.src = src;
     this.width = width;
@@ -174,11 +174,11 @@ function addImagem(src, posX, posY, width, height) {
     this.posX = posX;
     this.posY = posY;  
 
-    this.update = function() {
+    this.update = function() { //função de define a posição da imagem no canva
        c.drawImage(this.image, this.posX, this.posY, this.width, this.height);
     }
 }
-function personagem(src, posX, posY, width, height, dy, dx, y) {
+function personagem(src, posX, posY, width, height, dy, dx, y) { 
     this.image = new Image();
     this.image.src = src;
     this.width = width;
@@ -262,7 +262,7 @@ function personagem(src, posX, posY, width, height, dy, dx, y) {
         var posicaoY = this.posY;
         var largura = this.width;
         var altura = this.height;
-        var parametro = 6;
+        var parametro = 3;
         objColisao.forEach(function(item,indice){
             if((posicaoX+largura >= objColisao[indice].posX) && (posicaoX <= objColisao[indice].posX+objColisao[indice].width) && (posicaoY+altura > objColisao[indice].posY) && (posicaoY+altura <= objColisao[indice].posY+parametro)){
                 colisaoChao = true;
@@ -299,10 +299,11 @@ function personagem(src, posX, posY, width, height, dy, dx, y) {
     }
  } 
     
-function animate(){
+function animate(){ //função que desenha no canva
     requestAnimationFrame(animate);
     c.clearRect(0,0,1350,610);
 
+    //usando o método update() do objeto addImagem para desenhar as imagens declaradas acima no canvas
     imgBackground.update();
     sorveteria.update();
     chao.update();
@@ -328,5 +329,5 @@ function animate(){
     Helena.andar();
     Helena.pular();
 }
-animate();
+animate(); //chamando a função animate 
 
