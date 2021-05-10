dde953157912cfddbee1d6c68175a47a366f5cca
Virginia Leticia <vlafonso@hotmail.com>
2021-05-10 17:47:52 -0300

    Add comentarios codigo principal

--- Programacao/codigo_principal/Helena.js
+++ Programacao/codigo_principal/Helena.js
@@ -1,30 +1,29 @@
-var canvas = document.querySelector('canvas');
-canvas.width = 1350;
-canvas.height = 610;
-var c = canvas.getContext('2d');
-var cenario = 1;
+var canvas = document.querySelector('canvas'); //chama o canvas do html
+canvas.width = 1350; //define largura canvas
+canvas.height = 610; //define tamanho canvas
+var c = canvas.getContext('2d'); //define o contexto do canvas para 2d
+var cenario = 1; //controle dos cenários
 
-const button = document.querySelector('button')
-button.addEventListener('click', function(){
+const botaoSom = document.querySelector('button') //adiciona o audio
+botaoSom.addEventListener('click', function(){
     const audio = document.querySelector('audio')
     audio.play()
-    console.log("Tocando")
 })
 
-var tecla;
-var shift;
+var tecla; //define variaveis captura teclas
+var shift; 
 var tecla_anterior;
 
-var pulando = false;
-var gravidade = 3;
-var hPulo = 0;
+var pulando = false; //define variavel controle pulo
+var gravidade = 3; //define variavel gravidade
+var hPulo = 0; //define variavel controle altura pulo
 
-var colisaoChao = false;
+var colisaoChao = false; //define variaveis colisoes
 var colisaoCima = false;
 var colisaoEsquerda = false;
 var colisaoDireita = false;
 
-var quantidadeMoedas = 1;
+var quantidadeMoedas = 1; //define variaveis quantidade moedas, poções e vidas
 var quantidadePocoes = 3;
 var quantidadeVidas = 3;
 
@@ -37,9 +36,9 @@ var sprites = [
     "sprites_helena_ex/correr_e.png",
     "sprites_helena_ex/pulo_1.png",
     "sprites_helena_ex/pulo_2.png",
-];
+]; //define array sprites Helena
 
-var sorveteria;
+var sorveteria; //define variaveis obejtos cenario
 var chao;
 var chao1;
 var chao2;
@@ -61,24 +60,24 @@ var serra1;
 var serra2;
 var objColisao;
 var imgBackground = new addImagem("sprites_cenario/BG.png", 0, 0, canvas.width, canvas.height);//define a imagem de backgraund com o mesmo tamanho do canvas
-
-window.addEventListener('keydown', function(event) {
+//coloca imagem background no canva com posições definidadas pelo addImagem
+window.addEventListener('keydown', function(event) { //verifica teclas pressionadas
 tecla = event.key;
 shift = event.shiftKey;
 tecla_anterior = tecla;
 })
-window.addEventListener('keyup', function(event) {
+window.addEventListener('keyup', function(event) { //verifica teclas soltas
     tecla = '';
 })
 
-function apareceCenario(){
+function apareceCenario(){ //controla imagem que devem aparecer em cada cenario
     if (cenario == 1){
         sorveteria = new addImagem("sprites_cenario/sorveteria.png",-50,110,587,441 );//coloca o sprite da sorveteria no canva com posições definidadas pelo objeto addImagem
         chao = new addImagem("sprites_cenario/chao1.png",0,482,640,128);//coloca o sprite do chão no canva com posições definidadas pelo objeto addImagem
         caixa = new addImagem("sprites_cenario/Crate.png",574,418,64,64);//coloca o sprite da caixa no canva com posições definidadas pelo objeto addImagem
         plat = new addImagem("sprites_cenario/plat.png",672,290,192,64);//coloca o sprite da plataforma1 no canva com posições definidadas pelo objeto addImagem
         plat1 = new addImagem("sprites_cenario/plat1.png",1158,322,192,64);//coloca o sprite da plataforma2 no canva com posições definidadas pelo objeto addImagem
-        pico = new addImagem("sprites_cenario/pico.png",960,140,64,256);//coloca o sprite do "pico" no canva com posições definidadas pelo objeto addImagem
+        pico = new addImagem("sprites_cenario/pico.png",960,150,64,256);//coloca o sprite do "pico" no canva com posições definidadas pelo objeto addImagem
         arv = new addImagem("sprites_cenario/Tree_2.png",380,226,256,256);//coloca o sprite da árvore no canva com posições definidadas pelo objeto addImagem
         arb = new addImagem("sprites_cenario/Bush (1).png",380,430,106.4,52);//coloca o sprite do arbusto no canva com posições definidadas pelo objeto addImagem
         objColisao = [chao, caixa, plat, pico, plat1];//varável que armazena em um array todas as variáveis de sprite declaradas acima, essas variáveis representam objetos com colisão
@@ -110,13 +109,13 @@ function apareceCenario(){
 }
 
 
-var coracao1 = new addImagem("hud_sprites/cora_cheio.png", 1100, 60, 50, 50);
+var coracao1 = new addImagem("hud_sprites/cora_cheio.png", 1100, 60, 50, 50); //adiciona imagens do hud (corações, poção e moeda)
 var coracao2 = new addImagem("hud_sprites/cora_cheio.png", 1125, 60, 50, 50);
 var coracao3 = new addImagem("hud_sprites/cora_cheio.png", 1150, 60, 50, 50);
 var pocaoCura = new addImagem("hud_sprites/pocao.png", 1190, 51, 80, 80);
 var moeda= new addImagem("hud_sprites/moeda.png", 1087, 20, 80, 80);
 
-function textos(){
+function textos(){ //adiciona textos do hud (quantidade moedas e poções) e de fim de jogo
     c.font = '18px Georgia';
     c.fillText("x" +quantidadeMoedas, 1145, 55);
 
@@ -143,11 +142,11 @@ function moedas(){
     
         tecla = ' ';
         }
-        }
+    }
 }
 
 function dano(){
-        //representa tomar dano
+    //representa tomar dano, alterando sprites de vida
     if(quantidadeVidas > 0){
         quantidadeVidas -= 1;
 
@@ -178,19 +177,16 @@ function tomarPocao(){
                 coracao3 = new addImagem("hud_sprites/cora_cheio.png", 1150, 60, 50, 50);
                 coracao3.update();
                 tecla = ' ';
-                console.log("Vida:", quantidadeVidas);
             }
             else if(quantidadeVidas == 2){
                 coracao2 = new addImagem("hud_sprites/cora_cheio.png", 1125, 60, 50, 50);
                 coracao2.update();
                 tecla = ' ';
-                console.log("Vida:", quantidadeVidas);
             }      
             else if(quantidadeVidas == 1){
                 coracao1 = new addImagem("hud_sprites/cora_cheio.png", 1100, 60, 50, 50);
                 coracao1.update();
                 tecla = ' ';
-                console.log("Vida:", quantidadeVidas);
             } 
           }
         }
@@ -202,7 +198,7 @@ function tomarPocao(){
         }
 }
 
-function addImagem(src, posX, posY, width, height) {
+function addImagem(src, posX, posY, width, height) { //função para adicionar imagens
     this.image = new Image();
     this.image.src = src;
     this.width = width;
@@ -215,10 +211,10 @@ function addImagem(src, posX, posY, width, height) {
     }
 }
 
-var Helena = new personagem(sprites[0],148,356,83,122,0)
+var Helena = new personagem(sprites[0],148,356,83,122,0); //criacao personagem Helena
 
-function personagem(src, posX, posY, width, height,dy) {
-    this.image = new Image();
+function personagem(src, posX, posY, width, height,dy) { //funcao criaao personagens
+    this.image = new Image(); //define parametros personagem
     this.image.src = src;
     this.width = width;
     this.height = height;   
@@ -226,12 +222,12 @@ function personagem(src, posX, posY, width, height,dy) {
     this.posY = posY;
     this.dy = dy;
 
-    this.desenhar = function() {
+    this.desenhar = function() { //adiciona imagem personagem na tela
     c.drawImage(this.image, this.posX, this.posY, this.width, this.height,);
     }
 
-    this.andar = function () {
-        if (tecla == ''){
+    this.andar = function () { //controla movimentos esquerda e direita personagem
+        if (tecla == ''){ //altera sprite parada em relação ao movimento anterior (esquerda ou direita)
             this.image.src = sprites[0];
             if ((tecla_anterior == 'a')||(tecla_anterior == 'A')){
                 this.image.src = sprites[1];
@@ -240,8 +236,8 @@ function personagem(src, posX, posY, width, height,dy) {
                 this.image.src = sprites[7];
             }
         }
-        if(shift==true){
-            if(colisaoDireita==false){
+        if(shift==true){ //verifica se shift está ativado, para movimento de corrida
+            if(colisaoDireita==false){ //se não tiver colisão, corre para esquerda ou direita
                 if((tecla=='a') || (tecla=='A')) {
                     this.posX = this.posX - 10;
                     this.image.src = sprites[5] 
@@ -254,8 +250,8 @@ function personagem(src, posX, posY, width, height,dy) {
                     }
             }
         }
-        else{
-            if(colisaoDireita==false){
+        else{ //movimento de andar
+            if(colisaoDireita==false){//se não tiver colisão, anda para esquerda ou direita
                 if((tecla=='a') || (tecla=='A')) {
                     this.posX = this.posX - 3;
                     this.image.src = sprites[3]
@@ -272,93 +268,93 @@ function personagem(src, posX, posY, width, height,dy) {
         colisaoDireita = false;
     }
 
-    this.pular = function () {
-        if((tecla=='w')||(tecla=='W')){
-            if(pulando==false){
+    this.pular = function () { //controla pulo personagem
+        if((tecla=='w')||(tecla=='W')){ 
+            if(pulando==false){ //se não estiver pulando, "ativa" o pulo
                 pulando = true;
             }
         }
-        if ((pulando==true)&&(colisaoCima==false)){
+        if ((pulando==true)&&(colisaoCima==false)){ //se não houver colisão e houver pulo ativado, pula 
             pulando = true;
             this.dy = -7;
             hPulo += 1;
             this.image.src = sprites[6]
-            if(hPulo >= 42.85){
-                pulando = false;
+            if(hPulo >= 42.85){ //verifica se atingiu altura máx de pulo
+                pulando = false; //para de pular
                 hPulo = 0;
             }
         }
-        this.posY += gravidade+this.dy; 
+        this.posY += gravidade+this.dy; //adiciona gravidade ao movimento no eixo Y
         this.dy = 0;
         colisaoCima = false;
     }
 
-    this.colisao = function(){
-        var posicaoX = this.posX;
+    this.colisao = function(){ //verifica colisões
+        var posicaoX = this.posX; //criação de variáveis para utilização no forEach
         var posicaoY = this.posY;
         var largura = this.width;
         var altura = this.height;
-        var parametro = 3;
-        objColisao.forEach(function(item,indice){
+        var parametro = 7; //parametro de colisao (quanto a mais verifica de colisão)
+        objColisao.forEach(function(item,indice){ //verifica colisao com todos os objetos que podem ter colisao
             if((posicaoX+largura >= objColisao[indice].posX) && (posicaoX <= objColisao[indice].posX+objColisao[indice].width) && (posicaoY+altura > objColisao[indice].posY) && (posicaoY+altura <= objColisao[indice].posY+parametro)){
-                colisaoChao = true;
-                if((indice==6)||(indice==7)||(indice==8)){
-                    dano();
+                colisaoChao = true; //verifica se há colisão com o chão
+                if(((indice==6)||(indice==7)||(indice==8))&&(cenario==2)){ //verifica se a colisao ocorreu com objetos que dão dano (serras e espinhos)
+                    dano(); //chama a função de dano
                 }
             }
             if((posicaoX+largura >= objColisao[indice].posX) && (posicaoX <= objColisao[indice].posX+parametro) && (posicaoY+altura > objColisao[indice].posY) && (posicaoY <= objColisao[indice].posY+objColisao[indice].height)||((posicaoX+largura>=canvas.width)&&(cenario==3))){
                 colisaoEsquerda = true;
-                if((indice==6)||(indice==7)||(indice==8)){
-                    dano();
+                if(((indice==6)||(indice==7)||(indice==8))&&(cenario==2)){ //verifica se a colisao ocorreu com objetos que dão dano (serras e espinhos)
+                    dano(); //chama a função de dano
                 }
             }
             if((posicaoX <= objColisao[indice].posX+objColisao[indice].width)&&(posicaoX >= objColisao[indice].posX+objColisao[indice].width-parametro)&&(posicaoY+altura > objColisao[indice].posY) && (posicaoY <= objColisao[indice].posY+objColisao[indice].height)||((posicaoX<=0)&&(cenario==1))){
                 colisaoDireita = true;
-                if((indice==6)||(indice==7)||(indice==8)){
-                    dano();
+                if(((indice==6)||(indice==7)||(indice==8))&&(cenario==2)){ //verifica se a colisao ocorreu com objetos que dão dano (serras e espinhos)
+                    dano(); //chama a função de dano
                 }
             }
-            if((posicaoX+largura >= objColisao[indice].posX) && (posicaoX <= objColisao[indice].posX+objColisao[indice].width) && (posicaoY < objColisao[indice].posY+objColisao[indice].height)&& (posicaoY >= objColisao[indice].posY+objColisao[indice].height-parametro)||((posicaoY<=0)&&(indice<6))){
+            if((posicaoX+largura >= objColisao[indice].posX) && (posicaoX <= objColisao[indice].posX+objColisao[indice].width) && (posicaoY < objColisao[indice].posY+objColisao[indice].height)&& (posicaoY >= objColisao[indice].posY+objColisao[indice].height-parametro)||(posicaoY<=0)){
                 colisaoCima = true;
-                if((indice==6)||(indice==7)||(indice==8)){
-                    dano();
+                if(((indice==6)||(indice==7)||(indice==8))&&(cenario==2)){ //verifica se a colisao ocorreu com objetos que dão dano (serras e espinhos)
+                    dano(); //chama a função de dano
                 }
             }
         });
-        if(colisaoChao == true){
-            gravidade = 0;
+        if(colisaoChao == true){ //verifica se houve colisao com o chao
+            gravidade = 0; //não há influencia da gravidade
         }
         else{
-            gravidade = 3;
+            gravidade = 3; //gravidade influencia
         }
         colisaoChao = false;
     }
-    this.trocaCenario = function(){
-        if((cenario == 1)&&(this.posX>=canvas.width)){
-            cenario = 2;
+    this.trocaCenario = function(){ //altera os cenarios de acordo com a movimentação
+        if((cenario == 1)&&(this.posX>=canvas.width)){ //sai do cenario 1 se passar pelo lado direito da tela
+            cenario = 2; //passa para o cenário 2
             this.posX = 0;
         }
-        if((cenario == 2)&&(this.posX>=canvas.width)){
-            cenario = 3;
+        if((cenario == 2)&&(this.posX>=canvas.width)){ //sai do cenario 2 se passar pelo lado direito da tela
+            cenario = 3; //passa para o cenário 3
             this.posX = 0;
         }
-        if(((cenario == 2)&&(this.posX+this.width<=0))){
-            cenario = 1;
+        if(((cenario == 2)&&(this.posX+this.width<=0))){ //sai do cenario 2 se passar pelo lado esquerdo da tela
+            cenario = 1; //passa para o cenário 1
             this.posX = canvas.width-this.width;
         }
-        if(((cenario == 3)&&(this.posX+this.width<=0))){
-            cenario = 2;
+        if(((cenario == 3)&&(this.posX+this.width<=0))){ //sai do cenario 3 se passar pelo lado esquerdo da tela
+            cenario = 2; //passa para o cenário 2
             this.posX = canvas.width-this.width;
         }
     }
  } 
     
-function animate(){
-    requestAnimationFrame(animate);
-    c.clearRect(0,0,1350,610);
-    apareceCenario();
-    imgBackground.update();
-    if(cenario == 1){
+function animate(){ //funcao de animação
+    requestAnimationFrame(animate); //chama novamente a função de animação
+    c.clearRect(0,0,1350,610); //apaga os objetos 
+    apareceCenario(); //apresenta os objetos do cenário
+    imgBackground.update(); //apresenta o background
+    if(cenario == 1){ //chama as funções que apresentam os objetos do cenário 1
         sorveteria.update();
         chao.update();
         caixa.update();
@@ -368,7 +364,7 @@ function animate(){
         plat1.update();
         pico.update();
     }
-    if(cenario == 2){
+    if(cenario == 2){ //chama as funções que apresentam os objetos do cenário 2
         plat1.update();
         plat2.update();
         plat3.update();
@@ -379,7 +375,7 @@ function animate(){
         serra1.update();
         serra2.update();
     }
-    if(cenario == 3){
+    if(cenario == 3){ //chama as funções que apresentam os objetos do cenário 3
         chao.update();
         arv1.update();
         arv2.update();
@@ -390,23 +386,19 @@ function animate(){
         placa.update();
     }
 
-    coracao1.update();
+    coracao1.update(); //chama as funções que apresentam os objetos do hud
     coracao2.update();
     coracao3.update();
     moeda.update();
     pocaoCura.update();
-    textos();
-    moedas();
-    tomarPocao();
+    textos(); //chama as funções que apresentam os textos do hud
+    moedas(); //chama a função que controla as moedas
+    tomarPocao(); //chama a função que controla o uso de poção
     
-    Helena.desenhar();
-    Helena.colisao();
-    Helena.andar();
-    Helena.pular();
-    Helena.trocaCenario();
+    Helena.desenhar(); //chama a função que apresenta a Helena
+    Helena.colisao(); //chama a função que verifica a colisao da Helena
+    Helena.andar(); //chama a função que realiza os movimentos da Helena
+    Helena.pular(); //chama a função que realiza o pulo da Helena
+    Helena.trocaCenario(); //chama a função que troca o cenario
 }
-animate();
-
-function mudaCenario(nCenario){
-    cenario = nCenario;
-}
+animate(); //chama a função de animação
